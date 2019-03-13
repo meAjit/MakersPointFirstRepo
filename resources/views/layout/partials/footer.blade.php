8b728f3531c4dc26abcc17f38afc47fa4fc1d6fd
Susanna Jalo <susanna.jalo@gmail.com>
2019-03-13 15:11:33 +0200

    Changed link color while on page
    I still cannot understand why the footer works on the pages I made but not on all of them.

--- resources/views/layout/partials/footer.blade.php
+++ resources/views/layout/partials/footer.blade.php
@@ -1,11 +1,12 @@
 <!-- Div that is included in every page 2/2 -->	
-<div>
+
+</div>
 <footer class="text-muted">
 
-     <div class="container-fluid">
+  
 
-		<p class="align-text-bottom"> Turku University of Applied Sciences</p>
+		<p class="align-text-bottom">Turku University of Applied Sciences</p>
 
-     </div>
+
 
    </footer>
